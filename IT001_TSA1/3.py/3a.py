d8522c5969660409354d165c38d286a864126137
Marielle05 <167722588+Marielle05@users.noreply.github.com>
2025-02-03 00:59:09 +0800

    Delete IT001_TSA1 directory

--- IT001_TSA1/3.py/3a.py
+++
@@ -1,5 +0,0 @@
-def pattern_for(n):
-    for i in range(1, n + 1):
-        print(" " * (n - i) + "".join(str(j) for j in range(1, i + 1)))
-
-pattern_for(5)
--- IT001_TSA1/3.py/3b.py
+++
@@ -1,8 +0,0 @@
-def pattern_while(n):
-    i = 1
-    while i <= n:
-        if i % 2 == 1:
-            print(str(i) * i)
-        i += 1
-
-pattern_while(7)
