5f0140b0a59edfc42c3a2823734673f807733143
Marielle05 <marielle121305@gmail.com>
2025-02-11 01:06:38 +0800

    refactor(midterm): added code

---
+++ MIDTERM/midterm_num_2.py
@@ -0,0 +1,12 @@
+from datetime import datetime
+
+date_input = input("Enter the date (mm/dd/yyyy): ")
+
+try:
+    date_obj = datetime.strptime(date_input, "%m/%d/%Y")
+    
+    formatted_date = date_obj.strftime("%B %d, %Y")
+    
+    print("Date Output:", formatted_date)
+except ValueError:
+    print("Invalid date format! Please enter the date in mm/dd/yyyy format.")
