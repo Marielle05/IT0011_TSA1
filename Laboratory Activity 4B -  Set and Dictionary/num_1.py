00dbfa815da8801250379b3f869c9e0826bea120
Marielle05 <marielle121305@gmail.com>
2025-02-27 01:55:35 +0800

    refactor(lab4b): added code

--- Laboratory Activity 4B -  Set and Dictionary/num_1.py
+++ Laboratory Activity 4B -  Set and Dictionary/num_1.py
@@ -5,23 +5,23 @@ C = {'c', 'd', 'f', 'h', 'i', 'j', 'k'}
 
 # a. Elements in A and B (Intersection)
 A_intersect_B = A & B
-print("Elements in A and B:", A_intersect_B, "Count:", len(A_intersect_B))
+print("A. Elements in A and B:", sorted(A_intersect_B), "Count:", len(A_intersect_B))
 
 # b. Elements in B but not in A and C
 B_exclusive = B - (A | C)
-print("Elements in B but not in A and C:", B_exclusive, "Count:", len(B_exclusive))
+print("B. Elements in B but not in A and C:", sorted(B_exclusive), "Count:", len(B_exclusive))
 
 # c. Showing specific sets using set operations
-set_1 = C - (A | B)  # {h, i, j, k}
-set_2 = (A & B & C) | (A & B)  # {c, d, f}
-set_3 = (A & B) | (B & C)  # {b, c, h}
-set_4 = A & B & C  # {d, f}
-set_5 = {'c'}  # {c}
-set_6 = B - (A | C)  # {l, m, o}
+set_1 = sorted(C - (A | B))  # [h, i, j, k]
+set_2 = sorted((A & B & C) | (A & B))  # [c, d, f]
+set_3 = sorted((A & B) | (B & C))  # [b, c, d, f]
+set_4 = sorted(A & B & C)  # [c, d, f]
+set_5 = ['c']  # [c]
+set_6 = sorted(B - (A | C))  # [l, m, o]
 
-print("Set [h, i, j, k]:", set_1)
-print("Set [c, d, f]:", set_2)
-print("Set [b, c, h]:", set_3)
-print("Set [d, f]:", set_4)
-print("Set [c]:", set_5)
-print("Set [l, m, o]:", set_6)
+print("i.", set_1)
+print("ii.", set_2)
+print("iii.", set_3)
+print("iv.", set_4)
+print("v.", set_5)
+print("vi.", set_6)
